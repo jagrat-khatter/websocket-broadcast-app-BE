df62969983337587547a7a279648c3cf9e5f51ec
jagrat-khatter <jagratkhatter@gmail.com>
2025-09-28 17:46:12 +0000

    Some correction for deploymeent

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,83 @@
-import { WebSocketServer  , WebSocket}  from 'ws';
+import z from 'zod'
+import {WebSocketServer , WebSocket} from 'ws'
+import dotenv from 'dotenv';
+dotenv.config();
 
-const wss = new WebSocketServer({port : 8080})
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const wss = new WebSocketServer({ port });
 // This is a JSON string — literally just a sequence of characters following the JSON format.
 // Whether it’s HTTP or WebSocket, what travels over the network is always text (or binary, but JSON is text).
 
 let userCount: number= 0;
-let allSockets : WebSocket[] = []
 // the type of this parameter socket is WebSocket we are importing this from ws bcoz we have to use 
 // WebSocket from this library bcoz we are using event listener from this library it
 // is also natively present in JS
 const rooms = new Map()
+
+const joinSchema = z.object({
+    type : z.literal('join') ,
+    roomId: z.string() ,
+})
+
+const chatSchema = z.object({
+    type: z.literal('chat'),
+    user: z.string(),
+    text: z.string(),
+})
 wss.on('connection' , (socket)=>{
-    let currentRoom : String | null= null;
+    let currentRoom : string | null= null;
     
+    console.log("Someone Connected")
     // after connection is established first time currentRoom will be null
     // after first  request to join room currentRoom will be setup and changed from null
     // map with key as currentRoom will have all the sockets corresponding to that roomId
     socket.on('message' , (message)=>{
         // converting binary or buffer data to normal JSON string this is extra step that HTTP
         const msgString = message.toString();
+        let data: unknown;
         // JSON string to JS object
-        const data = JSON.parse(msgString);
-        if(data.type === "join"){
-            currentRoom = data.roomId;
-            if(!rooms.has(currentRoom)){
-                rooms.set(currentRoom , new Set());
-            }
-
-            rooms.get(currentRoom).add(socket);
-            console.log(`Currently number members in rooms ${currentRoom} : ${rooms.get(currentRoom)!.size}`);
-            socket.send(`You have joined room ${currentRoom}`);
-            // room has been added to that room id
+        try{
+            data = JSON.parse(msgString);
         }
-        else if(data.type === "chat"){
-            const array: WebSocket[] = rooms.get(currentRoom);
+        catch(err){
+            console.log("Error parsing JSON" , err);
+            socket.send("Invalid JSON format");
+            return ;
+        }
 
-            for(const s of array){
-                if(s != socket) s.send(JSON.stringify({user: data.user , text: data.text}))
-            }
+        if(typeof data === 'object' && data !== null && 'type' in data)
+        {
+                if(data.type === "join"){
+                const result = joinSchema.safeParse(data);
+                if(!result.success) {
+                    socket.send("Invalid join payload");
+                    return ;
+                }
+                currentRoom = result.data.roomId;
+                if(!rooms.has(currentRoom)){
+                    rooms.set(currentRoom , new Set());
+                }
+
+                rooms.get(currentRoom).add(socket);
+                console.log(`Currently number members in rooms ${currentRoom} : ${rooms.get(currentRoom)!.size}`);
+                socket.send(`You have joined room ${currentRoom}`);
+                // room has been added to that room id
+                }
+                else if(data.type === "chat" && currentRoom!==null){
+                    const result = chatSchema.safeParse(data);
+                    if(! result.success) {
+                        socket.send("Invalid chat payload");
+                        return ;
+                    }
+                    const array: WebSocket[] = Array.from(rooms.get(currentRoom));
+
+                    for(const s of array){
+                        if(s != socket) s.send(JSON.stringify({user: result.data.user , text: result.data.text}))
+                    }
+                }
         }
     })
+    
     socket.on('close' , ()=>{
         if(currentRoom && rooms.get(currentRoom)){
             rooms.get(currentRoom).delete(socket) ;
